b66f9dd970a78334a2d2b7167518bca3dddb6945
igus1 <igus1695922408@gmail.com>
2026-06-21 22:48:12 +0300

    ДЗ1

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    String name = "Ирина";
+            int age = 34;
+    String favoriteLanguage = "Java";
+
+    System.out.println("Имя: " + name);
+    System.out.println("Возраст: " + age);
+    System.out.println("Любимый язык программирования " + favoriteLanguage);
+        }
+    }
