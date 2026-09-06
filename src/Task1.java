d8e3e4e205f690566839471e187081c111ed52bf
igus1 <igus1695922408@gmail.com>
2026-09-06 12:24:44 +0300

    ДЗ1.3

---
+++ src/Task1.java
@@ -0,0 +1,10 @@
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Ирина");
+        System.out.println(123 * 456);
+        System.out.println("$$$$$$$$$$");
+        System.out.println("Добрый ");
+        System.out.println("день");
+        System.out.println("=) ");
+    }
+}
